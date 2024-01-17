d86e4c7920f66eb2e47c05ea8830ef6a16fbcb7a
HAMIMTHOYYIB <hamimthoyyib@gmail.com>
2024-01-17 22:16:01 +0530

    last changes

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
 const session = require('express-session');
-
-
+ 
 const app = express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// setting the session middleware
 app.use(session({
-  secret: 'your-secret-key',
+  secret: 'secret-key',
   resave: false,
   saveUninitialized: true,
 }));
 
-// Connect to MongoDB 
+// connecting to mongodb database named ex-crud-database
 mongoose.connect('mongodb://localhost:27017/ex-crud-database', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB connected to the app'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// User Schema structure of document
+  
+// structure for a document (schema)
 const User = mongoose.model('User', {
     username: String,
     email: String,
@@ -32,27 +32,27 @@ const User = mongoose.model('User', {
 });
   
 
-// Serve the login page
+  //                                         routes
+
 app.get('/', async (req, res) => {
   try {
-    // Check if the user is already logged in by checking the session
-    if (req.session && req.session.username) {
+    // to check if the user has loggedin , if it is show loggedin page 
+    if (req.session && req.session.username) { // checking the username where if the user is not deleted
       res.redirect('/loggedin?username=' + req.session.username);
     } else {
-      // const usersCount = await User.countDocuments();
-      // if (usersCount === 0) {
-        // res.redirect('/signup');
-      // } else {
+      // if not show the loggin page
         res.render('login', { message: '' });
-      // }
     }
   } catch (error) {
     res.status(500).json({ message: 'Error checking users', error });
   }
 });
 
+app.get('/login', (req, res) => {
+  res.render('login' , {message : ''});
+});
 
-// Handle login form submission
+
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     try {
@@ -64,30 +64,29 @@ app.post('/login', async (req, res) => {
       if (!passwordMatch) {
         return res.render('login', { message: 'Incorrect password. Please try again.' });
       }
-      req.session.username = user.username;
+      req.session.username = user.username;  //session is created here
       res.redirect('/loggedin?username=' + user.username);
     } catch (error) {
       res.status(500).json({ message: 'Login failed', error });
     }
   });
 
-// Serve the signup page
+
 app.get('/signup', (req, res) => {
   let errorMessage = '';
   res.render('signup', { errorMessage })
 });
 
-// Handle signup form submission
 app.post('/signup', async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      let errorMessage = '';
-      if (existingUser.username === username) {
-        errorMessage = 'Username already exists. Please choose another username.';
-      }
-      return res.render('signup', { errorMessage });
+        let errorMessage = '';
+        if (existingUser.username === username) {
+          errorMessage = 'Username already exists.Choose another username.';
+        }
+        return res.render('signup', { errorMessage });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
@@ -106,7 +105,7 @@ app.post('/signup', async (req, res) => {
 app.get('/loggedin', async (req, res) => {
   try {
     if (!req.session || !req.session.username) {
-      return res.redirect('/'); // Redirect to login if session doesn't exist
+      return res.redirect('/'); // Redirect to login if session there is no session
     }
     const { username } = req.query;
     const user = await User.findOne({ username });
@@ -123,18 +122,16 @@ app.get('/loggedin', async (req, res) => {
 app.post('/delete', async (req, res) => {
     try {
       const { username } = req.body;
-      console.log("username : " , req.body);
       const deletedUser = await User.findOneAndDelete({ username });
       if (!deletedUser) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'User not found for delete' });
       }
-      res.redirect('/login'); // Redirect to the login page after successful deletion
+      res.redirect('/login'); // redirect to login page after deletion
     } catch (error) {
-      res.status(500).json({ message: 'Error deleting user', error });
+      res.status(500).json({ message: 'Error on deleting user', error });
     }
   });
 
-  // Add/Edit User Form
   app.get('/edit', async (req, res) => {
     try {
       const { username } = req.query;
@@ -149,8 +146,6 @@ app.post('/delete', async (req, res) => {
     }
   });  
 
-
-// Handle Edit User form submission
 app.post('/edit', async (req, res) => {
   try {
     const { username, email, currentPassword, newPassword } = req.body;
@@ -159,25 +154,22 @@ app.post('/edit', async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
     
-    // Validate current password
+    // check the current password is true or not
     const passwordMatch = await bcrypt.compare(currentPassword, user.password);
     if (!passwordMatch) {
-      return res.redirect(`/edit?username=${username}&error=Current%20password%20is%20incorrect.`);
-      // Redirect to the edit page with the error message as a query parameter
+      return res.redirect(`/edit?username=${username}&error=Current password is incorrect.`);
     }
 
-    // Update email if changed
+    //update the password and mail if it is changed
     if (email !== user.email) {
       user.email = email;
     }
-
-    // Update password if new password provided
     if (newPassword) {
       const hashedPassword = await bcrypt.hash(newPassword, 10);
       user.password = hashedPassword;
     }
+
     await user.save();
-    // res.redirect(`/loggedin?username=${username}`);
     res.render('loggedin', { username, email });
     
   }  catch (error) {
@@ -186,15 +178,8 @@ app.post('/edit', async (req, res) => {
 });
 
 
-
-
-app.get('/login', (req, res) => {
-    res.render('login' , {message : ''});
-  });
-
-// Handle logout
 app.get('/logout', (req, res) => {
-  // Destroy the session on logout
+// remove the session  when logout
   req.session.destroy((err) => {
     if (err) {
       console.error('Error destroying session:', err);
@@ -203,10 +188,8 @@ app.get('/logout', (req, res) => {
   });
 });
 
-  
-
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on: http://localhost:${PORT}`);
+//  server running
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on: http://localhost:${port}`);
 });
